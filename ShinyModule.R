e1bd52109c205909165c3b247a393fe25cb525bd
andreakoelzsch <akoelzsch@ab.mpg.de>
2023-03-21 13:50:07 +0100

    first version simple

--- ShinyModule.R
+++ ShinyModule.R
@@ -1,5 +1,7 @@
 library("shiny")
-library("move")
+library("shinyWidgets")
+library("move2")
+library("dplyr")
 
 # to display messages to the user in the log file of the App in MoveApps
 # one can use the function from the src/common/logger.R file:
@@ -8,8 +10,11 @@ library("move")
 shinyModuleUserInterface <- function(id, label) {
   ns <- NS(id) ## all IDs of UI functions need to be wrapped in ns()
   tagList(
-    titlePanel("Plot of Track"),
-    numericInput(inputId = ns("ind"), label = "Select one indivdual", value = 1),
+    titlePanel("Plot of selected Non-Location Measure"),
+      fluidRow(
+      column(3,uiOutput(ns("uiind"))),
+      column(3,uiOutput(ns("uiattr")))
+      ),
     plotOutput(ns("plot"))
   )
 }
@@ -17,9 +22,25 @@ shinyModuleUserInterface <- function(id, label) {
 shinyModule <- function(input, output, session, data){ ## The parameter "data" is reserved for the data object passed on from the previous app
   ns <- session$ns ## all IDs of UI functions need to be wrapped in ns()
   current <- reactiveVal(data)
+
+  output$uiattr <- renderUI({
+    dataNNA <- data[,!sapply(data, function(x) all(is.na(x)))]
+    #dataNUM <- dataNNA[,!sapply(as.list(dataNNA), function(x) is.numeric(x))]
+  selectInput(ns("attr"),"Select attribute", choices=names(dataNNA))
+  })
+  
+  output$uiind <- renderUI({
+    selectInput(ns("ind"),"Select track", choices=unique(mt_track_id(data)),selected=mt_track_id(data)[1])
+  })
   
   output$plot <- renderPlot({
-    plot(data[[input$ind]])
+    #if (is.null(input$attr))
+    #{
+    #  plot(0,0,type="n",xlab="timestamp",ylab="",axes=FALSE)
+    #} else
+    #{
+      plot(mt_time(data[mt_track_id(data)==input$ind,]),as.data.frame(data[mt_track_id(data)==input$ind,input$attr])[,1],type="l",col=4,xlab="timestamp",ylab=input$attr)
+    #}
   })
   
   return(reactive({ current() })) ## if data are not modified, the unmodified input data must be returned
--- co-pilot-sdk.R
+++ co-pilot-sdk.R
@@ -1,7 +1,7 @@
 ## Provided testing datasets in `./data/raw`: 
 ## "input1_greylgeese.rds", "input2_whitefgeese.rds", "input3_stork.rds", "input4_goat.rds"  
 ## for own data: file saved as a .rds containing a object of class MoveStack
-inputFileName = "./data/raw/input2_whitefgeese.rds" 
+inputFileName = "./data/raw/example_acc_2geese.rds" 
 ## optionally change the output file name
 unlink("./data/output/", recursive = TRUE) # delete "output" folder if it exists, to have a clean start for every run
 dir.create("./data/output/") # create folder for output
